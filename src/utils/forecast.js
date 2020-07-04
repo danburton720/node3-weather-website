df5b05c5a8e4481bc7677d6b6a9f58011cbdb3b7
Dan Burton <danburton720@hotmail.com>
2020-07-04 16:06:39 +0100

    Add humidity reading to weather response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,6 +17,7 @@ const forecast = (latitude, longitude, callback) => {
                 + '. It is currently ' + body.current.temperature
                 + ' degrees out. It feels like '
                 + body.current.feelslike + ' degrees out.'
+                + ' The humidity is ' + body.current.humidity + '%.'
             )
         }
     })
